df81c5064cd81bffe934184c2274a39b0f0f22d9
Mehmet OSMANCIK <mzekiosmancik@outlook.com>
2023-02-07 15:12:27 -0600

    init

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 67);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(208, 16);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 23);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Minutes to Jiggle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Times jiggled";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(178, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 160);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Jiggle Jiggle  :D ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/bin/Debug/netcoreapp3.1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool doJiggle;
+        private Thread t1;
+        decimal minute = 0;
+        int jiggleCount = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doJiggle = true;
+            t1 = new System.Threading.Thread(DoTheJiggle);
+            t1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            doJiggle = false;
+            jiggleCount = 0;
+            label3.Text = "0";
+
+        }
+
+        private void DoTheJiggle()
+        {   
+            while (doJiggle)
+            {
+                jiggleCount++;
+                Invoke(new Action(() =>
+                {
+                    label3.Text = Convert.ToString(jiggleCount);
+                }));
+                
+                System.Drawing.Point pt = System.Windows.Forms.Cursor.Position;
+                int moveUp = 0;
+                Random rnd = new Random();
+                int movement = rnd.Next(0, 3);
+                switch (movement)
+                {
+                    case 0:
+                        moveUp = -2;
+                        break;
+                    case 1:
+                        //Move up
+                        moveUp = -2;
+                        break;
+                    case 2:
+                        //Move down
+                        moveUp = 2;
+                        break;
+                    case 3:
+                        //Move down
+                        moveUp = 2;
+                        break;
+                }
+                // pt.Y += moveUp;
+                //System.Windows.Forms.Cursor.Position = pt;
+                mouse_event(MOUSEEVENTF_MOVE, 0, moveUp, 0, UIntPtr.Zero);
+               
+
+                System.Threading.Thread.Sleep(Convert.ToInt32(minute)*   // minutes to sleep
+                                              60 *   // seconds to a minute
+                                              1000);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(Int32 dwFlags, Int32 dx, Int32 dy, Int32 dwData, UIntPtr dwExtraInfo);
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            minute = numericUpDown1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            minute = numericUpDown1.Value;
+        }
+
+    }
+    
+
+}
